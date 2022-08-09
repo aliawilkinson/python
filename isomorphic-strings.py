69b0ab0625e732b7dcb009b17a330a97437346b2
Alia Wilkinson <awilkinson@loandepot.com>
2022-08-08 17:59:20 -0700

    isometric strings

--- isomorphic-strings.py
+++ isomorphic-strings.py
@@ -1,33 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if (len(s) != len(t)):
-             return False
-        map = {}
-        for i in range(0,len(s)):    
-            if(not s[i] in map):
-                if(not t[i] in map):
-                    map[s[i]] = t[i]
-                    print(map)
-                else:
-                    return False
-            elif (map[s[i]] != t[i]):
+        
+        mapping_s_t = {}
+        mapping_t_s = {}
+        
+        for c1, c2 in zip(s, t):
+            # No mapping exists in either of the dictionaries
+            if (c1 not in mapping_s_t) and (c2 not in mapping_t_s):
+                mapping_s_t[c1] = c2
+                mapping_t_s[c2] = c1
+            
+            # Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
+            # it doesn't match in either of the dictionaries or both            
+            elif mapping_s_t[c1] != c2 or mapping_t_s[c2] != c1:
                 return False
+            
         return True
-"""
-Given two strings s and t, determine if they are isomorphic.
-Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
-d e
-d g
-
-map = {
-b:b
-a:a
-d:b
-}
-
-
-
-"""
-
