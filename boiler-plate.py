a57b91a2566660e9040af7e08aaad25d13e4acd3
aliawilkinson <aliawilkinson@gmail.com>
2022-09-09 11:31:38 -0700

    improving boiler code

--- boiler-plate.py
+++ boiler-plate.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def task(self, nums: List[int], target_sum: int) -> bool:
+    def task(self, nums: List[int]) -> bool:
         """
         Task: 
             <Task>
@@ -11,23 +11,31 @@ class Solution:
             <Examples>
         """
 
-    def testTask(self, nums=List[int], answer=[int]) -> bool:
+    def testTask(self) -> bool:
         test_cases = {
+            0: {
+                "Input":"",
+                "Output":""
+                },
+            1: {
+                "Input":"",
+                "Output":""
+                }
         }
         passed = 0
         failed = 0
         for i in test_cases:
-            code_answer = self.task(test_cases[i], i)
-            if test_cases[i] == code_answer:
+            code_answer = self.task(test_cases[i]["Input"])
+            if test_cases[i]["Output"] == code_answer:
                 passed += 1
                 print("Success!, code answer was: ", code_answer,
-                      " right answer was: ", test_cases[i])
+                " right answer was: ", test_cases[i]["Output"])
             else:
                 failed += 1
                 print("Failed, code answer was: ", code_answer,
-                      " right answer was ", test_cases[i])
+                " right answer was ", test_cases[i]["Output"])
         print("Passed ", passed, " tests, Failed ", failed,
-              " tests, success rate: ", (passed/(passed+failed))*100, "%")
+                " tests, success rate: ", (passed/(passed+failed))*100, "%")
 
 
 task = Solution()
