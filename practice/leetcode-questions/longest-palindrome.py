f89a6f6daaf4e344a7f0e7825ea7af7353c23968
aliawilkinson <aliawilkinson@gmail.com>
2022-09-09 13:32:41 -0700

    longest palindrome more pseudocode

--- practice/leetcode-questions/longest-palindrome.py
+++ practice/leetcode-questions/longest-palindrome.py
@@ -32,76 +32,19 @@ class Solution:
         if track[2]
 
         """
-        # track = {}
-        # # current center, right, left
-        # right = len(s) - 1
-        # middle = int((len(s) - 1) / 2)
-        # longest = 0
-        # for left in range(len(s)):
-        #     # get left val
-        #     if (not s[left] in track):
-        #         track[s[left]] = list()
-        #         track[s[left]].append({left:left+1})
-        #     else:
-        #         # then we already know that there is 1 in there
+        # look at the letter
+        # if the letter next to it is the same, check on either side of those letters
+        # two same letters are a palindrome
+        # if the next letters to the left and the right are different, return 
+        # else, continue
+        # if the new palindrome is longer than the last one set it to longest
+        # else, go until the end of the string 
+        # return longest palindrome
 
-        #         track[s[left]].append({left:left+1})
-        #     track[left] = s[left]
-
-        #     if (not s[right] in track):
-        #         track[s[right]] = list()
-        #         track[s[right]].append({right:right-1})
-        #     else:
-        #         # then we already know there is 1
-        #         track[s[right]].append({right:right-1})
-        #     track[right] = s[right]
-        #     # get right val
-        #     # get next to vals
-        #     # if both those vals are on the same num, cont
-
-        #     # move left towards middle
-        #     right = right-1
-
-        # for middle in range
-
-        middle = int((len(s) - 1) / 2)
-        right = middle - 1
-        left = middle + 1
-        palindromes = list()
-        longest = str()
-        current_pal = str()
-        for i in range(middle):
-            # check values next to middle
-            # "babad", 2:b is middle
-            right, left = self.initPointers(middle)
-            # 1:a and 3:a
-            if (s[right] == s[left]):
-                current_pal = s[right:left+1]
-                right, left = self.movePointers(right, left)
-                self.checkPointers(s,right,left)
-            else:
-                if (len(longest) == len(s)):
-                    break
-                if (len(longest) > len(current_pal)):
-                    longest = current_pal
-                # new middle
-                middle = right
-                self.initPointers(middle)
-                right, left = self.movePointers(right, left)
-            
-            if (len(longest) < len(current_pal)):
-                    longest = current_pal
-        return longest
-
-        # we start in the middle because that's where the palindrome will be the longest 
-        # if we have a large number palindrome and the index number to either edge is shorter, we skip
-
-# Input: s = "babad"
-#         Output: "bab"
-#         Explanation: "aba" is also a valid answer.
-# look at 2:b
-# look at 3:a,1:a
-# same, so add right-left+1
+        # more considerations: 
+            # can start from the middle since those could be the longest 
+            # if we have a palindrome that is the same length as the str, return 
+            # if we have a palindrome that is already longer than any more could be, return 
 
 
     def checkPointers(self, s, right, left):
