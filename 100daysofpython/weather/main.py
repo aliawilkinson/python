79f375ff2b6c1c93a1c91de7fca8d0c1aa5b4ba2
aliawilkinson <aliawilkinson@gmail.com>
2023-05-24 11:37:11 -0600

    day25 weather

--- 100daysofpython/weather/main.py
+++ 100daysofpython/weather/main.py
@@ -1,5 +1,7 @@
 # day 25
 
+# challenge 1
+
 # weather_set = []
 # with open('weather_data.csv') as weather:
 #     for l in weather.readlines():
@@ -7,13 +9,41 @@
 
 # print(weather_set)
 
-import csv
+# challenage 2
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    next(data)
-    for row in data:
-        print(int(row[1]))
+# import csv
 
-        # create a new temperatures list, contains all temps inside weather data
-        # int
+# def get_temp():
+#   with open("weather_data.csv") as data_file:
+#       data = csv.reader(data_file)
+#       next(data)
+#       temperatures = []
+#       for row in data:
+#           temperatures.append(int(row[1]))
+#       return temperatures
+#         # create a new temperatures list, contains all temps inside weather data
+#         # int
+
+# temperatures = get_temp()
+# print(temperatures)
+
+# challenge 3
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
+data_dict = data.to_dict()
+print(data_dict)
+temp_list = data["temp"].to_list()
+print("temp_list",temp_list)
+
+# average temp in temp list
+average = sum(temp_list) / len(temp_list)
+print("average temp",average)
+
+print("temp mean", data["temp"].mean())
+
+# use data series method to find the max temp
+
+print("temp max", data["temp"].max())
